1dd30ee438f7e77a6cfd6a0b19da4d7d99dda5bd
Radoslaw <zerek.r93@gmail.com>
2021-08-05 17:48:49 +0200

    Initial commit

---
+++ src/main/java/com/radoslawzerek/bigbetfrontend/BigBetFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.radoslawzerek.bigbetfrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigBetFrontendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BigBetFrontendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/radoslawzerek/bigbetfrontend/BigBetFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.radoslawzerek.bigbetfrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigBetFrontendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
